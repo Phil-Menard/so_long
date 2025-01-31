f56cc273ae99e7e8eebe2f13b751cfc1ca78758f
Philippe Menard <phil.menard1668@gmail.com>
2025-01-31 16:30:19 +0100

    displays white background and two pixel squares

--- so_long.h
+++ so_long.h
@@ -6,23 +6,45 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:25:00 by pmenard           #+#    #+#             */
-/*   Updated: 2025/01/30 16:36:08 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/01/31 14:44:47 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+#  define RED_PIXEL 0xFF0000
+#  define GREEN_PIXEL 0xFF00
+#  define WHITE_PIXEL 0xFFFFFF
+
 # include "libft/libft.h"
 # include "mlx_linux/mlx.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp; //bits_per_pixel
+	int		line_len;
+	int		endian;
+}				t_img;
 
 typedef struct s_data
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+	void	*mlx;
+	void	*window;
+	t_img	img;
+}	t_data;
 
 #endif
