1a0893895f0ed1e654ebd638917721365edb6f00
Philippe Menard <phil.menard1668@gmail.com>
2025-02-06 17:35:45 +0100

    everything works fine, need to handle collision with ennemy

--- srcs/ennemy.c
+++ srcs/ennemy.c
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 15:02:26 by pmenard           #+#    #+#             */
-/*   Updated: 2025/02/06 16:33:51 by pmenard          ###   ########.fr       */
+/*   Updated: 2025/02/06 17:25:32 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,55 +49,11 @@ int	move_ennemy_y_axis(t_game *game, int x, int y)
 
 void	move_ennemy(t_game *game)
 {
-	int	i;
-	int	j;
-	int	previous;
+	int	x;
+	int	y;
 
-	i = 0;
-	previous = -1;
-	while (i < game->map.rows)
-	{
-		j = 0;
-		while (j < game->map.columns)
-		{
-			if (game->map.full_map[i][j] == ENNEMY)
-			{
-				ft_printf("column : %s\n", game->map.full_map[i]);
-				game->ennemy.pos_x = j;
-				game->ennemy.pos_y = i;
-				if (j != previous)
-					previous = move_ennemy_y_axis(game, i, j);
-			}
-			j++;
-		}
-		i++;
-	}
+	x = game->ennemy.pos_x;
+	y = game->ennemy.pos_y;
+	move_ennemy_y_axis(game, y, x);
+
 }
-
-/* void	move_ennemy_x_axis(t_game *game, int x, int y)
-{
-	if (game->ennemy.is_going_left == 1)
-	{
-		if (game->map.full_map[y][x - 1] != WALL
-			&& game->map.full_map[y][x - 1] != EXIT)
-		{
-			change_sprites(game, &(game->floor), &(game->ennemy));
-			--game->ennemy.pos_x;
-			change_sprites(game, &(game->ennemy), &(game->ennemy));
-		}
-		else
-			game->ennemy.is_going_left = 0;
-	}
-	else
-	{
-		if (game->map.full_map[y][x + 1] != WALL
-			&& game->map.full_map[y][x - 1] != EXIT)
-		{
-			change_sprites(game, &(game->floor), &(game->ennemy));
-			++game->ennemy.pos_x;
-			change_sprites(game, &(game->ennemy), &(game->ennemy));
-		}
-		else
-			game->ennemy.is_going_left = 1;
-	}
-} */
